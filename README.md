b4168de328c2a5fcc202536a0a33c21cfe024bc6
BelisMAU <belismau@gmail.com>
2020-12-29 17:48:24 +0100

    Updated to work on raspberry pi with respeaker

--- README.md
+++ README.md
@@ -1,16 +1,19 @@
 # Audio recorder
 
-Record audio with ```pyaudio``` on a Raspberry Pi and send the created wav-file to a server using ```ZMQ```
+Record audio with ```pyaudio``` on a Raspberry Pi (with ```ReSpeaker 4-Mic Array```) and send the created wav-file to a server using ```ZMQ```
 
 ## Installation
 
 ```$ git clone https://github.com/belismau/audio-recorder```
+
 ```$ cd audio-recorder```
+
 ```$ sudo pip install keyboard```
+
 ```$ sudo apt-get install python-pyaudio```
 
 ## Run
 
 ```$ sudo python app.py```
 
-**Note:** When starting recording, debug messages will pop up. Simply, ignore them.
+**Note:** When starting recording, debug messages will pop up. Simply, ignore them. Also, use a keyboard that is plugged into your Raspberry. Otherwise it won't detect keyboard inputs.
--- app.py
+++ app.py
@@ -1,17 +1,32 @@
 import pyaudio
 import keyboard
 import wave
-from client import send_audio
+#from client import send_audio
 
 class Recorder():
     def __init__(self, filename):
-        self.audio_format   = pyaudio.paInt16
-        self.channels       = 2
-        self.sample_rate    = 44100
-        self.chunk          = int(0.03*self.sample_rate)
-        self.filename       = filename
-        self.start          = 's'
-        self.quit           = 'q'
+        self.audio_format    = pyaudio.paInt16
+        self.channels        = 2
+        self.sample_rate     = 44100
+        self.chunk           = int(0.03*self.sample_rate)
+        self.filename        = filename
+        self.start           = 's'
+        self.quit            = 'q'
+    
+    def get_index(self):
+        p = pyaudio.PyAudio()
+        info = p.get_host_api_info_by_index(0)
+        numdevices = info.get('deviceCount')
+         
+        for i in range(0, numdevices):
+            if (p.get_device_info_by_host_api_device_index(0, i).get('maxInputChannels')) > 0:
+                device_name = p.get_device_info_by_host_api_device_index(0, i).get('name')
+                if device_name == 'ac108':
+                    self.respeaker_index = i
+                    return True
+        
+        return False
+                    
 
     def record(self):
         recorded_data = []
@@ -21,6 +36,7 @@ class Recorder():
                         channels            = self.channels,
                         rate                = self.sample_rate, 
                         input               = True,
+                        input_device_index  = self.respeaker_index,
                         frames_per_buffer   = self.chunk)
 
         print('\nRecording STARTED...')
@@ -54,6 +70,7 @@ class Recorder():
 
 audio_file = 'audio.wav'
 recorder = Recorder(audio_file)
+recorder.get_index()
 recorder.listen()
 
 #...then send to server...
